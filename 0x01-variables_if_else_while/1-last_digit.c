b9adbae6bee23eeabfe7973a99423a2a44f0f872
Amine S <amineblack21@gmail.com>
2023-02-18 12:26:09 +0100

    update

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,32 +1,3 @@
-Skip to content
-Search or jump to…
-Pull requests
-Issues
-Codespaces
-Marketplace
-Explore
- 
-@techGuy008 
-Irenimoyan2
-/
-alx-low_level_programming
-Public
-Fork your own copy of Irenimoyan2/alx-low_level_programming
-Code
-Issues
-Pull requests
-Actions
-Projects
-Security
-Insights
-alx-low_level_programming/0x01-variables_if_else_while/1-last_digit.c
-
-Irenimoyan2 1-last_digit.c
-Latest commit 21ec88e 11 hours ago
- History
- 0 contributors
-Executable File  33 lines (27 sloc)  414 Bytes
-
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
