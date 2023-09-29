360834eee1a29f1abe7b412b977af16f827e9e8d
Amine S <a.saroui21@gmail.com>
2023-09-30 00:56:10 +0100

    update

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+/**
+ * _puts_recursion - print string with new line
+ */
+void _puts_recursion(char *str)
+{
+    if(*str == '\0')
+    {
+        return;
+    }
+    printf("%c", *str);
+    _puts_recursion(++str);
+}
