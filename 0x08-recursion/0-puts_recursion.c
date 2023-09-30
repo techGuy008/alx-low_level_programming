d79751953a27eeb244805a693fb56f86a2c04d85
Amine S <a.saroui21@gmail.com>
2023-09-30 01:15:33 +0100

    update

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 /**
  * _puts_recursion - print string with new line
+ * @s: string to print
  */
-void _puts_recursion(char *str)
+
+void _puts_recursion (char *str)
 {
-    if(*str == '\0')
-    {
-        return;
-    }
-    printf("%c", *str);
-    _puts_recursion(++str);
+if (*str == '\0')
+{
+return;
+}
+printf("%c", *str);
+_puts_recursion(++str);
 }
