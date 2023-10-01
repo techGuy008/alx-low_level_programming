226564601823d8eb1661fe3b49e2dbb8a5860eee
Amine S <a.saroui21@gmail.com>
2023-10-01 20:36:13 +0100

    update

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,22 +9,19 @@ int wildcmp(const char *str1, const char *str2)
 {
 if (*str1 == '\0' && *str2 == '\0')
 {
-return 1;
+return (1);
 }
 if ((*str1 == '\0' || *str2 == '\0') && (*str1 != '*' && *str2 != '*'))
 {
-return 0;
+return (0);
 }
 if (*str1 != '*' && *str2 != '*')
 {
 if (*str1 == *str2)
 {
-return wildcmp(str1 + 1, str2 + 1);
+return (wildcmp(str1 + 1, str2 + 1));
 }
-else
-{
-return 0;
-}
+return (0);
 }
 return (wildcmp(str1, str2 + 1) ||
 wildcmp(str1 + 1, str2) ||
