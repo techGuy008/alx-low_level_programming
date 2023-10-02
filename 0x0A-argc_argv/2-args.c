8852dbd4daf467f1acf3f9d5aba781c543ed0ef4
Amine S <a.saroui21@gmail.com>
2023-10-02 15:03:27 +0100

    update

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
 /**
+ * printArgs - prints all arguments it receives.
+ * @argc: argument counter
+ * @argv: pointer to array of arguments
+ */
+void printArgs(int argc, char *argv[])
+{
+if (argc > 0)
+{
+printf("%s\n", argv[0]);
+printArgs(argc - 1, argv + 1);
+}
+}
+/**
  * main - prints all arguments it receives.
  * @argc: argument counter
  * @argv: pointer to array of arguments
  * Return: 0 on success
  */
-
 int main(int argc, char *argv[])
 {
-for (int i = 0; i < argc; i++)
-{
-printf("%s\n", argv[i]);
-}
+printArgs(argc, argv);
 
 return 0;
 }
