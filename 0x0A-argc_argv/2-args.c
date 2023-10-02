e98539c239dafd2a7bfd8730c0639dec1c97c383
Amine S <a.saroui21@gmail.com>
2023-10-02 15:10:08 +0100

    update

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
-#include <stdlib.h>
-
 /**
- * main - multiplies two numbers
+ * printArgs - prints all arguments it receives.
+ * @argc: argument counter
+ * @argv: pointer to array of arguments
+ */
+void printArgs(int argc, char *argv[])
+{
+if (argc > 0)
+{
+printf("%s\n", argv[0]);
+printArgs(argc - 1, argv + 1);
+}
+}
+/**
+ * main - prints all arguments it receives.
  * @argc: argument counter
  * @argv: pointer to array of arguments
  * Return: 0 on success
  */
 int main(int argc, char *argv[])
 {
-if (argc != 3)
-{
-printf("Error\n");
-return (1);
-}
-
-int n1 = atoi(argv[1]);
-int n2 = atoi(argv[2]);
-
-int res = n1 * n2;
-
-printf("%d\n", res);
+printArgs(argc, argv);
 
 return (0);
 }
