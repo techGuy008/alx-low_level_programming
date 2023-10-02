7c0ce87bfc2e1ff7ee0fb97aa27015bd0a6bdf76
Amine S <a.saroui21@gmail.com>
2023-10-02 15:09:13 +0100

    update

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * printArgs - prints all arguments it receives.
+ * @argc: argument counter
+ * @argv: pointer to array of arguments
+ */
+void printArgs(int argc, char *argv[])
+{
+if (argc > 0)
+{
+printf("%s\n", argv[0]);
+printArgs(argc - 1, argv + 1);
+}
+}
+/**
+ * main - prints all arguments it receives.
+ * @argc: argument counter
+ * @argv: pointer to array of arguments
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+printArgs(argc, argv);
+
+return (0);
+}
