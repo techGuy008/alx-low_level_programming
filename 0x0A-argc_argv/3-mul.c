5c251e3a4900f3040c9b60093ddd208a5331c4fd
Amine S <a.saroui21@gmail.com>
2023-10-03 01:22:33 +0100

    update

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ * Return: int result
+ */
+int _atoi(const char *str)
+{
+int res = 0;
+int sign = 1;
+while (*str == ' ' || *str == '\t')
+{
+str++;
+}
+
+if (*str == '-')
+{
+sign = -1;
+str++;
+}
+else if (*str == '+')
+{
+str++;
+}
+
+while (*str >= '0' && *str <= '9')
+{
+res = res * 10 + (*str - '0');
+str++;
+}
+
+return res * sign;
+}
+
 /**
  * main - multiplies two numbers
  * @argc: argument counter
@@ -15,8 +47,8 @@ printf("Error\n");
 return (1);
 }
 
-int n1 = atoi(argv[1]);
-int n2 = atoi(argv[2]);
+int n1 = _atoi(argv[1]);
+int n2 = _atoi(argv[2]);
 
 int res = n1 * n2;
 
