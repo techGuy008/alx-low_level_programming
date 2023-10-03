15bf231af6962828c6161f47b8dae271f41b37b4
Amine S <a.saroui21@gmail.com>
2023-10-03 02:13:23 +0100

    update

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,30 +10,30 @@
 
 int main(int argc, char *argv[])
 {
-    if (argc < 2)
-    {
-        printf("0\n");
-        return 0;
-    }
+if (argc < 2)
+{
+printf("0\n");
+return 0;
+}
 
-    int sumVal = 0;
+int sumVal = 0;
 
-    for (int i = 1; i < argc; i++)
-    {
-        char *arg = argv[i];
-        for (int j = 0; arg[j] != '\0'; j++)
-        {
-            if (!isdigit(arg[j]))
-            {
-                printf("Error\n");
-                return 1;
-            }
-        }
-        int n = atoi(arg);
-        sumVal += n;
-    }
+for (int i = 1; i < argc; i++)
+{
+char *arg = argv[i];
+for (int j = 0; arg[j] != '\0'; j++)
+{
+if (!isdigit(arg[j]))
+{
+printf("Error\n");
+return 1;
+}
+}
+int n = atoi(arg);
+sumVal += n;
+}
 
-    printf("%d\n", sumVal);
+printf("%d\n", sumVal);
 
-    return 0;
+return 0;
 }
