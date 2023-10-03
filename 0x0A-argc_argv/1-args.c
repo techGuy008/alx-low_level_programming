4067e18aa4f8a67cde223fd2504af5e9949b44b4
Amine S <a.saroui21@gmail.com>
2023-10-03 01:02:38 +0100

    update

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,9 +5,9 @@
  * @argv: pointer to array of arguments
  * Return: 0 on success
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[]  __attribute__((unused)))
 {
-if (argc > 0)
+if (argc > 0 && argv[0] != NULL)
 {
 printf("%d", argc);
 }
