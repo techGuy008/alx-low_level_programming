d1c868a633a198c5d73883153d43625f19fbdb14
Amine S <a.saroui21@gmail.com>
2023-10-05 22:09:55 +0100

    update

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,22 +1,44 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * free_grid - frees a 2 dimensional grid
- * @grid: grid input
- * @height: height input
+ * argstostr -  concatenates all the arguments
+ * @ac: int input
+ * @av: double pointer array
+ *
+ * Return: 0
  */
 
-void free_grid(int **grid, int height)
+char *argstostr(int ac, char **av)
 {
-if (grid == NULL)
-{
-return;
-}
+    if (ac == 0 || av == NULL)
+    {
+        return NULL;
+    }
 
-for (int i = 0; i < height; i++)
-{
-free(grid[i]);
-}
+    int total = 0;
 
-free(grid);
+    for (int i = 0; i < ac; i++)
+    {
+        total += strlen(av[i]) + 1;
+    }
+
+    char *result = (char *)malloc(total + 1);
+
+    if (result == NULL)
+    {
+        return NULL;
+    }
+
+    int pos = 0;
+
+    for (int i = 0; i < ac; i++)
+    {
+        strcpy(result + pos, av[i]);
+        pos += strlen(av[i]);
+        result[pos++] = '\n';
+    }
+
+    result[pos] = '\0';
+
+    return result;
 }
