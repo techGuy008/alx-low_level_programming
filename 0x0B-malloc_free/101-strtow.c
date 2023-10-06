428504fd45cb45a243a8c130c7fe09947c7232cb
Amine S <a.saroui21@gmail.com>
2023-10-06 02:37:16 +0100

    update

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,13 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+/**
+ * free_all - frees the memory
+ * @string: pointer values being passed for freeing
+ * @i: counter
+ */
 void free_all(char **string, int i)
 {
 for (; i > 0;)
 free(string[--i]);
 free(string);
 }
-
+/**
+ * count - function to calculate number of words
+ * @str: string being passed to check for words
+ *
+ * Return: number of words
+ */
 int count(char *str)
 {
 int a, num = 0;
@@ -26,7 +36,11 @@ num++;
 }
 return (num);
 }
-
+/**
+ * strtow - function that splits string into words
+ * @str: string being passed
+ * Return: null if string is empty or null or function fails
+ */
 char **strtow(char *str)
 {
 int total = 0, b = 0, c = 0, length = 0;
