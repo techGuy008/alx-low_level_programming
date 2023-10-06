01c1d3bd9d6ef7f45b10c69ae06eea6a42c4bc4d
Amine S <a.saroui21@gmail.com>
2023-10-06 02:19:52 +0100

    update

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -8,9 +8,11 @@ void free_all(char **string, int i)
 		free(string[--i]);
 	free(string);
 }
+
 int count(char *str)
 {
 int a, num = 0;
+
 for (a = 0; str[a] != '\0'; a++)
 {
 if (*str == ' ')
@@ -18,7 +20,8 @@ str++;
 else
 {
 for (; str[a] != ' ' && str[a] != '\0'; a++)
-str++;num++;
+str++;
+num++;
 }
 }
 return (num);
@@ -27,11 +30,15 @@ char **strtow(char *str)
 {
 int total = 0, b = 0, c = 0, length = 0;
 char **words, *found_word;
-if (str == 0 || *str == 0) return (NULL);
+
+if (str == 0 || *str == 0)
+return (NULL);
 total = count(str);
-if (total == 0) return (NULL);
+if (total == 0)
+return (NULL);
 words = malloc((total + 1) * sizeof(char *));
-if (words == 0) return (NULL);
+if (words == 0)
+return (NULL);
 for (; *str != '\0' && b < total;)
 {
 if (*str == ' ')
@@ -47,16 +54,20 @@ str++;
 words[b] = malloc((length + 1) * sizeof(char));
 if (words[b] == 0)
 {
-free_all(words,b);
+free_all(words, b);
 return (NULL);
 }
 while (*found_word != ' ' && *found_word != '\0')
 {
 words[b][c] = *found_word;
-found_word++;c++;
+found_word++;
+c++;
 }
 words[b][c] = '\0';
-b++;c = 0;length = 0;str++;
+b++;
+c = 0;
+length = 0;
+str++;
 }
 }
 return (words);
