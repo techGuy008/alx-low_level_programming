559d96b27c6a8eb29468454327732ee747f8cea8
Amine S <a.saroui21@gmail.com>
2023-10-09 23:54:09 +0100

    update

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,14 +11,14 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-    unsigned int i;
+unsigned int i;
 
-    for (i = 0; i < n; i++)
-    {
-        s[i] = b;
-    }
+for (i = 0; i < n; i++)
+{
+s[i] = b;
+}
 
-    return (s);
+return (s);
 }
 
 /**
@@ -30,17 +30,19 @@ char *_memset(char *s, char b, unsigned int n)
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-    char *pointer;
+char *pointer;
 
-    if (nmemb == 0 || size == 0)
-        return (NULL);
+if (nmemb == 0 || size == 0)
+{
+return (NULL);
+}
+pointer = malloc(size * nmemb);
 
-    pointer = malloc(size * nmemb);
+if (pointer == NULL)
+{
+return (NULL);
+}
+_memset(pointer, 0, nmemb * size);
 
-    if (pointer == NULL)
-        return (NULL);
-
-    _memset(pointer, 0, nmemb * size);
-
-    return (pointer);
+return (pointer);
 }
