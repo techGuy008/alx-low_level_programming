8ce3bf8ebef4befa2c197576fc1748efaee32a5d
Amine S <a.saroui21@gmail.com>
2023-10-10 13:11:44 +0100

    update

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -58,43 +58,48 @@ exit(98);
  */
 int main(int argc, char *argv[])
 {
-char *s1, *s2;
-int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
-
-s1 = argv[1], s2 = argv[2];
-if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+if (argc != 3 || !is_digit(argv[1]) || !is_digit(argv[2]))
 {
 errors();
 }
+int len1, len2, len, *result, digit1, carry, j, digit2;
+char *s1, *s2;
+*s1 = argv[1];
+*s2 = argv[2];
 len1 = _strlen(s1);
 len2 = _strlen(s2);
 len = len1 + len2 + 1;
 result = malloc(sizeof(int) * len);
+
 if (!result)
 {
-return (1);
+return 1;
 }
-for (i = 0; i <= len1 + len2; i++)
+
+for (int i = 0; i <= len1 + len2; i++)
 {
 result[i] = 0;
 }
-for (len1 = len1 - 1; len1 >= 0; len1--)
+
+for (int i = len1 - 1; i >= 0; i--)
 {
-digit1 = s1[len1] - '0';
+digit1 = s1[i] - '0';
 carry = 0;
-for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+for (j = len2 - 1; j >= 0; j--)
 {
-digit2 = s2[len2] - '0';
-carry += result[len1 + len2 + 1] + (digit1 * digit2);
-result[len1 + len2 + 1] = carry % 10;
+digit2 = s2[j] - '0';
+carry += result[i + j + 1] + (digit1 * digit2);
+result[i + j + 1] = carry % 10;
 carry /= 10;
 }
 if (carry > 0)
 {
-result[len1 + len2 + 1] += carry;
+result[i + len2 + 1] += carry;
 }
 }
-for (i = 0; i < len - 1; i++)
+
+int a = 0;
+for (int i = 0; i < len - 1; i++)
 {
 if (result[i])
 {
@@ -102,14 +107,16 @@ a = 1;
 }
 if (a)
 {
-_putchar(result[i] + '0');
+putchar(result[i] + '0');
 }
 }
+
 if (!a)
 {
-_putchar('0');
+putchar('0');
 }
-_putchar('\n');
+
+putchar('\n');
 free(result);
-return (0);
+return 0;
 }
